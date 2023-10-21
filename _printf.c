317b03cd28c7ff42c8a608bd55c47612ba200f9d
ochuks1 <cochuks@gmail.com>
2023-10-21 16:49:43 -0700

    printf

---
+++ _printf.c
@@ -0,0 +1,42 @@
+int _printf(const char *format, ...)
+{
+int printed_chars = 0;
+va_list args;
+va_start(args, format);
+while (*format)
+{
+if (*format != '%')
+{
+putchar(*format);
+printed_chars++;
+}
+else
+{
+format++;
+if (*format == 'c')
+{
+int character = va_arg(args, int);
+putchar(character);
+printed_chars++;
+}
+else if (*format == 's')
+{
+char *str = va_arg(args, char *);
+while (*str)
+{
+putchar(*str);
+printed_chars++;
+str++;
+}
+}
+else if (*format == '%')
+{
+putchar('%');
+printed_chars++;
+}
+}
+format++;
+}
+va_end(args);
+return printed_chars;
+}
