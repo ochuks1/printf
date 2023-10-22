c45351077f78f60028a5a12fd1b73d4c7eb5d00c
ochuks1 <cochuks@gmail.com>
2023-10-21 19:59:55 -0700

    told ya smile

--- printf.c
+++ printf.c
@@ -1,6 +1,8 @@
 #include "main.h"
 #include <stdarg.h>
 #include <unistd.h>
+#include <stdio.h>
+
 
 #define BUFFER_SIZE 1024
 
@@ -16,6 +18,19 @@ int _printf(const char *format, ...)
     int print_binary(va_list args);
     int print_unsigned(va_list args, char format);
     int print_str_non_printable(va_list args);
+    void itoa_hex(unsigned int n, char* buffer, char format);
+    void itoa_octal(unsigned int n, char* buffer);
+    void itoa_unsigned(unsigned int n, char* buffer);
+    int print_string(va_list args, int precision);
+     char *str = va_arg(args, char *);
+    int printed = 0;
+    int print_integer(va_list args, char format, int is_positive, int width);
+    int print_integer(va_list args, char format, int is_positive);
+    int n = va_arg(args, int);
+    char buffer[12];
+    itoa(n, buffer, is_positive);
+
+
 
     va_start(args, format);
     while (format && *format)
@@ -25,6 +40,53 @@ int _printf(const char *format, ...)
             format++;
             if (*format == '\0')
                 return (-1);
+
+	    if (precision >= 0)
+    {
+        int len = _strlen(str);
+        if (len > precision)
+        {
+            str = _strndup(str, precision);
+        }
+    }
+
+    printed += _puts(str);
+    return (printed);
+
+     if (format == '+' && n >= 0)
+    {
+        printed += _putchar('+');
+    }
+    else if (format == ' ' && n >= 0)
+    {
+        printed += _putchar(' ');
+    }
+
+    printed += _puts(buffer);
+    return (printed);
+
+     itoa(n, buffer, is_positive);
+    int printed = 0;
+
+    if (format == '+' && n >= 0)
+    {
+        printed += _putchar('+');
+        width--;
+    }
+    else if (format == ' ' && n >= 0)
+    {
+        printed += _putchar(' ');
+        width--;
+    }
+
+    while (width > 0)
+    {
+        printed += _putchar(' ');
+        width--;
+    }
+
+    printed += _puts(buffer);
+    return (printed);
 
             
 	    if (*format == 'c')
@@ -55,7 +117,35 @@ int _printf(const char *format, ...)
             buffer[i] = *format;
             count++;
             i++;
+
+	     while (format && format[i])
+    {
+        if (format[i] != '%')
+        {
+            putchar(format[i]);
+            count++;
         }
+
+	    else
+        {
+            i++;
+    
+        }
+
+	    else if (format[i] == 'u')
+{
+    count += print_unsigned(va_arg(args, unsigned int));
+}
+else if (format[i] == 'o')
+{
+    count += print_octal(va_arg(args, unsigned int));
+}
+else if (format[i] == 'x' || format[i] == 'X')
+{
+    count += print_hex(va_arg(args, unsigned int), format[i]);
+}
+
+
         format++;
     }
     if (i > 0)
@@ -63,4 +153,6 @@ int _printf(const char *format, ...)
 
     va_end(args);
     return count;
+return -1;
+
 }
