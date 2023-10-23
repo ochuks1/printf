ec06cc7fadfe449d5b8c45ffffeff8b7b3507f0f
ochuks1 <cochuks@gmail.com>
2023-10-22 18:56:48 -0700

    Task printf

--- printf.c
+++ printf.c
@@ -1,149 +1,124 @@
+#include <stdio.h>
 #include <stdarg.h>
 #include <unistd.h>
-#include <stdio.h>
 #include "main.h"
 
-#define BUFFER_SIZE 1024
-
 /**
- * _printf - Custom printf implementation with various format specifiers
- * @format: The format string
- * @...: Additional arguments for format specifiers
- *
- * Return: Number of characters printed (excluding the null byte)
+ * _printf - custom printf function.
+ * @format: format string
+ * Return: the number of characters printed
+ * (excluding the null byte used to end output to strings).
  */
 int _printf(const char *format, ...)
 {
 va_list args;
-int chars_printed = 0;
-char buffer[BUFFER_SIZE];
-int buffer_index = 0;
-char c;
-void handle_format_specifier(const char **format, va_list args, int *printed_chars);
+char buffer[1024];
+int char_count = 0;
+int i = 0;
 va_start(args, format);
-while ((c = *format++) != '\0')
+while (format[i] != '\0')
 {
-	if (c != '%')
-	{
-		buffer[buffer_index++] = c;
-		chars_printed++;
-	}
-	else
-	{
-		c = *format++;
-		switch (c)
-		{
-			case 'c':
-				buffer[buffer_index++] = va_arg(args, int);
-				chars_printed++;
-				break;
-			case 's':
-				{
-					const char *str = va_arg(args, const char *);
-					while (*str)
-					{
-						buffer[buffer_index++] = *str++;
-						chars_printed++;
-					}
-					break;
-				}
-			case '%':
-				buffer[buffer_index++] = '%';
-				chars_printed++;
-				break;
-			case 'd':
-			case 'i':
-				{
-					int num = va_arg(args, int);
-					int printed_chars = snprintf(&buffer[buffer_index], BUFFER_SIZE - buffer_index, "%d", num);
-					if (printed_chars > 0)
-					{
-						buffer_index += printed_chars;
-						chars_printed += printed_chars;
-					}
-					break;
-				}
-			case 'u':
-				{
-					unsigned int num = va_arg(args, unsigned int);
-					int printed_chars = snprintf(&buffer[buffer_index], BUFFER_SIZE - buffer_index, "%u", num);
-					if (printed_chars > 0)
+if (format[i] != '%')
+{
+buffer[char_count++] = format[i];
+if (char_count == 1023)
+{
+buffer[char_count] = '\0';
+write(1, buffer, char_count);
+char_count = 0;
+}
+}
+else
+{
+i++; /* Move past the '%' */
+if (format[i] == '\0')
+{
+break; /* Format string ends with a '%', so we break */
+if (format[i] == 'c')
+{
+char c = va_arg(args, int);
+buffer[char_count++] = c;
+}
+            else if (format[i] == 's')
+            {
+                const char *str = va_arg(args, const char *);
+                while (*str)
+                {
+                    buffer[char_count++] = *str++;
+                    if (char_count == 1023)
+                    {
+                        buffer[char_count] = '\0';
+                        write(1, buffer, char_count);
+                        char_count = 0;
+                    }
+                }
+            }
+            else if (format[i] == '%')
+            {
+                buffer[char_count++] = '%';
+            }
+            else if (format[i] == 'd' || format[i] == 'i')
+            {
+                int num = va_arg(args, int);
+                char_count += sprintf(buffer + char_count, "%d", num);
+            }
+            else if (format[i] == 'b')
+            {
+                unsigned int num = va_arg(args, unsigned int);
+                char_count += sprintf(buffer + char_count, "%b", num);
+            }
+            else if (format[i] == 'u')
+            {
+                unsigned int num = va_arg(args, unsigned int);
+                char_count += sprintf(buffer + char_count, "%u", num);
+            }
+            else if (format[i] == 'o')
+            {
+                unsigned int num = va_arg(args, unsigned int);
+                char_count += sprintf(buffer + char_count, "%o", num);
+            }
+            else if (format[i] == 'x' || format[i] == 'X')
+            {
+                unsigned int num = va_arg(args, unsigned int);
+                char_count += sprintf(buffer + char_count, format[i] == 'x' ? "%x" : "%X", num);
+            }
+            else if (format[i] == 'S')
+            {
+                const char *str = va_arg(args, const char *);
+                while (*str)
+                {
+                    if (*str >= 32 && *str < 127)
+                    {
+                        buffer[char_count++] = *str++;
+                    }
+                    else
+                    {
+                        char_count += sprintf(buffer + char_count, "\\x%02X", *str);
+                        str++;
+                    }
+                    if (char_count == 1023)
+                    {
+                        buffer[char_count] = '\0';
+                        write(1, buffer, char_count);
+                        char_count = 0;
+                    }
+                }
+            }
+        }
 
-					{
-						buffer_index += printed_chars;
-						chars_printed += printed_chars;
-					}
-					break;
-				}
-			case 'o':
-				{
-					unsigned int num = va_arg(args, unsigned int);
-					int printed_chars = snprintf(&buffer[buffer_index], BUFFER_SIZE - buffer_index, "%o", num);
-					if (printed_chars > 0)
+        if (char_count == 1023)
+        {
+            buffer[char_count] = '\0';
+            write(1, buffer, char_count);
+            char_count = 0;
+        }
+        i++;
+    }
+    buffer[char_count] = '\0';
+    write(1, buffer, char_count);
 
-					{
-						buffer_index += printed_chars;
-						chars_printed += printed_chars;
-					}
-					break;
-				}
-			case 'x':
-			case 'X':
-				{
-				unsigned int num = va_arg(args, unsigned int);
-				int printed_chars = snprintf(&buffer[buffer_index], BUFFER_SIZE - buffer_index, c == 'x' ? "%x" : "%X", num);
-				if (printed_chars > 0)
-
-				{
-					buffer_index += printed_chars;
-					chars_printed += printed_chars;
-				}
-					break;
-			case 'p':
-				{
-					void *ptr = va_arg(args, void *);
-					int printed_chars = snprintf(&buffer[buffer_index], BUFFER_SIZE - buffer_index, "%p", ptr);
-					if (printed_chars > 0)
-
-					{
-						buffer_index += printed_chars;
-						chars_printed += printed_chars;
-					}
-					break;
-				}
-			case 'e':
-			case 'E':
-				{
-				double num = va_arg(args, double);
-			 	int printed_chars = snprintf(&buffer[buffer_index], BUFFER_SIZE - buffer_index, c == 'e' ? "%e" : "%E", num);
-				if (printed_chars > 0)
-					{
-						buffer_index += printed_chars;
-						chars_printed += printed_chars;
-					}
-					break;
-				}
-/* Support for more format specifiers */
-			default:
-/* Unsupported format specifier, ignore it. */
-					break;
-					}
-			}
-			if (buffer_index >= BUFFER_SIZE)
-			
-			{
-				write(STDOUT_FILENO, buffer, buffer_index);
-				buffer_index = 0;
-			}
-		}
-		
-		if (buffer_index > 0)
-		
-		{
-			write(STDOUT_FILENO, buffer, buffer_index);
-		}
-		va_end(args);
-		return (chars_printed);
+    va_end(args);
+    return (char_count);
 }
 return -1;
 }
